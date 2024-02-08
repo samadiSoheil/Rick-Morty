f59f379f24804dff49e40bb60118d84b99079776
itsSoheil <soheil.samadi79@gmail.com>
2024-02-08 19:25:36 +0330

    Create Character Component

--- src/components/CharacterList.jsx
+++ src/components/CharacterList.jsx
@@ -1,3 +1,31 @@
-export default function CharacterList() {
-  return <div>Lorem, ipsum.</div>;
+import { EyeIcon } from "@heroicons/react/24/outline";
+
+export default function CharacterList({ allCharacters }) {
+  return (
+    <div className="character-list">
+      {allCharacters.map((character) => {
+        return <Character key={character.id} character={character} />;
+      })}
+    </div>
+  );
+}
+
+function Character({ character }) {
+  return (
+    <div key={character.id} className="list__item">
+      <img src={character.image} alt={character.name} />
+      <h3 className="name">
+        <span>{character.gender === "Male" ? "🧑" : "👩"}</span>
+        <span>{character.name}</span>
+      </h3>
+      <div className="list-item__info info">
+        <span className={`status ${character.status === "Dead" ? "red" : ""}`}></span>
+        <span> {character.status}</span>
+        <span> - {character.species}</span>
+      </div>
+      <button className="icon red">
+        <EyeIcon />
+      </button>
+    </div>
+  );
 }
