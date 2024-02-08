4d1342ca37e4c14dfb5eb625515687976883b6cd
itsSoheil <soheil.samadi79@gmail.com>
2024-02-08 19:24:36 +0330

    Send A Test Character Data To CharacterList

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import CharacterList from "./components/CharacterList";
 import CharacterDetail from "./components/CharacterDetail";
+import { allCharacters } from "../data/data";
 function App() {
   return (
     <div className="app">
       <Navbar />
       <div className="main">
-        <CharacterList />
+        <CharacterList allCharacters={allCharacters} />
         <CharacterDetail />
       </div>
     </div>
