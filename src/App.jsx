9d733f76520779eb442a097cdff3c4d37e16bc70
itsSoheil <soheil.samadi79@gmail.com>
2024-02-08 18:55:09 +0330

    Create CharacterList Document

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import CharacterList from "./components/CharacterList";
 
 function App() {
   return (
     <div className="app">
       <Navbar />
+      <div className="main">
+        <CharacterList />
+      </div>
     </div>
   );
 }
---
+++ src/components/CharacterList.jsx
@@ -0,0 +1,3 @@
+export default function CharacterList() {
+  return <div>Lorem, ipsum.</div>;
+}
