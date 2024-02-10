84ee88e518865005b0017030b431ec8f1686ed22
itsSoheil <soheil.samadi79@gmail.com>
2024-02-10 15:42:12 +0330

    Error Handling With react-hot-toast

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import CharacterList from "./components/CharacterList";
 import CharacterDetail from "./components/CharacterDetail";
 import { allCharacters } from "../data/data";
 import { useEffect, useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
 
 function App() {
   const [allCharacters, setAllCharacters] = useState([]);
@@ -13,13 +14,14 @@ function App() {
     async function fetchData() {
       try {
         setIsLoading(true);
-        let res = await fetch("https://rickandmortyapi.com/api/characterq");
+        let res = await fetch("https://rickandmortyapi.com/api/character");
         if (!res.ok) throw new Error("it is not find Characters information!");
         let data = await res.json();
         setAllCharacters(data.results.splice(0, 5));
-        setIsLoading(false);
       } catch (err) {
-        console.log(err);
+        toast.error(err.message);
+      } finally {
+        setIsLoading(false);
       }
     }
     fetchData();
@@ -27,6 +29,7 @@ function App() {
 
   return (
     <div className="app">
+      <Toaster />
       <Navbar />
       <div className="main">
         <CharacterList allCharacters={allCharacters} isLoading={isLoading} />
