534c90016961c21a9481369c953a9f31153acc70
itsSoheil <soheil.samadi79@gmail.com>
2024-02-10 20:17:28 +0330

    Creating query State For Search Characters

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import Navbar from "./components/Navbar";
+import Navbar, { Search } from "./components/Navbar";
 import CharacterList from "./components/CharacterList";
 import CharacterDetail from "./components/CharacterDetail";
 import { allCharacters } from "../data/data";
@@ -10,26 +10,32 @@ import axios from "axios";
 function App() {
   const [allCharacters, setAllCharacters] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [query, setQuery] = useState("");
 
   useEffect(() => {
     async function fetchData() {
       try {
         setIsLoading(true);
-        const { data } = await axios.get("https://rickandmortyapi.com/api/character");
-        setAllCharacters(data.results.splice(0, 5));
+        const { data } = await axios.get(
+          `https://rickandmortyapi.com/api/character?name=${query}`
+        );
+        setAllCharacters(data.results.splice(0));
       } catch (err) {
+        setAllCharacters([]);
         toast.error(err.response.data.error);
       } finally {
         setIsLoading(false);
       }
     }
     fetchData();
-  }, []);
+  }, [query]);
 
   return (
     <div className="app">
       <Toaster />
-      <Navbar />
+      <Navbar allCharactersLength={allCharacters.length}>
+        <Search query={query} setQuery={setQuery} />
+      </Navbar>
       <div className="main">
         <CharacterList allCharacters={allCharacters} isLoading={isLoading} />
         <CharacterDetail />
