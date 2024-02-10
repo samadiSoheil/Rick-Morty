cabb5d2ee1e253aa06cb01e1d3471fd6289f32c1
itsSoheil <soheil.samadi79@gmail.com>
2024-02-10 15:34:19 +0330

    Fetching Data And Send allCharacters To CharacterList

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,33 @@ import Navbar from "./components/Navbar";
 import CharacterList from "./components/CharacterList";
 import CharacterDetail from "./components/CharacterDetail";
 import { allCharacters } from "../data/data";
+import { useEffect, useState } from "react";
+
 function App() {
+  const [allCharacters, setAllCharacters] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        setIsLoading(true);
+        let res = await fetch("https://rickandmortyapi.com/api/characterq");
+        if (!res.ok) throw new Error("it is not find Characters information!");
+        let data = await res.json();
+        setAllCharacters(data.results.splice(0, 5));
+        setIsLoading(false);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    fetchData();
+  }, []);
+
   return (
     <div className="app">
       <Navbar />
       <div className="main">
-        <CharacterList allCharacters={allCharacters} />
+        <CharacterList allCharacters={allCharacters} isLoading={isLoading} />
         <CharacterDetail />
       </div>
     </div>
