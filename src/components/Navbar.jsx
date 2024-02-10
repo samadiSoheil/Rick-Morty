e0c8bb5c92f5ee8313b5febd7b43c8216dcc937b
itsSoheil <soheil.samadi79@gmail.com>
2024-02-10 20:16:05 +0330

    Change search JSX To Componnent

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,27 @@
 import { HeartIcon } from "@heroicons/react/24/outline";
 
-export default function Navbar() {
+export default function Navbar({ children, allCharactersLength }) {
   return (
     <nav className="navbar">
       <div className="navbar__logo">LOGO 😜</div>
-      <input type="text" className="text-field" placeholder="search.." />
-      <div className="navbar__result">Found X character</div>
+      {children}
+      <div className="navbar__result">Found {allCharactersLength} character</div>
       <button className="heart">
         <HeartIcon className="icon" />
         <span className="badge">4</span>
       </button>
     </nav>
+  );
+}
+
+export function Search({ query, setQuery }) {
+  return (
+    <input
+      value={query}
+      onChange={(e) => setQuery(e.target.value)}
+      type="text"
+      className="text-field"
+      placeholder="search character.."
+    />
   );
 }
