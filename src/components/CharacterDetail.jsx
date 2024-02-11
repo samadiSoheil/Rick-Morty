ba5254bed15173df698b838b7a4e8cd498ca8a15
itsSoheil <soheil.samadi79@gmail.com>
2024-02-11 21:22:29 +0330

    Fetch Single Character And it's Episodes

--- src/components/CharacterDetail.jsx
+++ src/components/CharacterDetail.jsx
@@ -1,28 +1,73 @@
 import { ArrowUpCircleIcon } from "@heroicons/react/24/outline";
 import { character, episodes } from "../../data/data";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-export default function CharacterDetail() {
+export default function CharacterDetail({ characterId }) {
+  // fetch Single Character
+  const [singleCharacter, setSingleCharacter] = useState(null);
+  const [allEpisode, setAllEpisode] = useState(null);
+
+  useEffect(() => {
+    async function fetchCharacter() {
+      try {
+        const { data } = await axios.get(
+          `https://rickandmortyapi.com/api/character/${characterId}`
+        );
+        // console.log(data);
+        setSingleCharacter(data);
+
+        // fetch episodes
+        const episodesArr = data.episode.map((e) => {
+          return e.split("/").at(-1);
+        });
+        // console.log(episodesArr);
+        const { data: episode } = await axios.get(
+          `https://rickandmortyapi.com/api/episode/${episodesArr}`
+        );
+        // console.log(episode);
+        episode ? setAllEpisode([episode].flat().splice(0)) : null;
+      } catch (error) {
+        toast.error(error.response.data.error);
+      }
+    }
+    characterId ? fetchCharacter() : null;
+  }, [characterId]);
+
+  if (!singleCharacter)
+    return (
+      <p
+        style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        className="character-detail name"
+      >
+        Select a character to see their details
+      </p>
+    );
+  if (!singleCharacter) return null;
+
   return (
     <div style={{ flex: 1 }}>
       <div className="character-detail">
         <img
-          src={character.image}
-          alt={character.name}
+          src={singleCharacter.image}
+          alt={singleCharacter.name}
           className="character-detail__img"
         />
         <div className="character-detail__info">
           <h3 className="name">
-            <span>{character.gender == "Male" ? "🧑" : "👩"}</span>
-            <span>{character.name}</span>
+            <span>{singleCharacter.gender == "Male" ? "🧑" : "👩"}</span>
+            <span>{singleCharacter.name}</span>
           </h3>
           <div className="info">
-            <span className={`status ${character.status === "Dead" ? "red" : ""}`}></span>
-            <span> {character.status}</span>
-            <span> - {character.species}</span>
+            <span
+              className={`status ${singleCharacter.status === "Dead" ? "red" : ""}`}
+            ></span>
+            <span> {singleCharacter.status}</span>
+            <span> - {singleCharacter.species}</span>
           </div>
           <div className="location">
             <p>Last known location</p>
-            <p>{character.location.name}</p>
+            <p>{singleCharacter.location.name}</p>
           </div>
           <div className="actions">
             <button className="btn btn--primary">Add to favorite</button>
@@ -35,17 +80,19 @@ export default function CharacterDetail() {
           <ArrowUpCircleIcon className="icon" />
         </div>
         <ul>
-          {episodes.map((e, index) => {
-            return (
-              <li key={e.id}>
-                <div>
-                  {String(index + 1).padStart(2, "0") + ` -`} {e.episode} :{" "}
-                  <strong>{e.name}</strong>
-                </div>
-                <div className="badge badge--secondary">{e.air_date}</div>
-              </li>
-            );
-          })}
+          {!allEpisode
+            ? null
+            : allEpisode.map((e, index) => {
+                return (
+                  <li key={e.id}>
+                    <div>
+                      {String(index + 1).padStart(2, "0") + ` -`} {e.episode} :{" "}
+                      <strong>{e.name}</strong>
+                    </div>
+                    <div className="badge badge--secondary">{e.air_date}</div>
+                  </li>
+                );
+              })}
         </ul>
       </div>
     </div>
